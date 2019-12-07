d29dfd16f1fb749618d39fbb6e98e6893c4451c3
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-07 09:34:43 +0000

    Automatically backed up by Learn

--- lib/test.rb
+++ lib/test.rb
@@ -4,12 +4,24 @@ collection = [
       { :title => "Movie C", :studio => "Omega Films", :worldwide_gross => 30 }
     ]
 
-new_hash = {}
-collection.length.times do |index|
-  if new_hash[collection[index][:studio]]
-    new_hash[collection[index][:studio]] += collection[index][:worldwide_gross]
-  else
-    new_hash[collection[index][:studio]] = collection[index][:worldwide_gross]
-  end
+def movie_with_director_name(director_name, movie_data)
+  { 
+    :title => movie_data[:title],
+    :worldwide_gross => movie_data[:worldwide_gross],
+    :release_year => movie_data[:release_year],
+    :studio => movie_data[:studio],
+    :director_name => director_name
+  }
 end
-p new_hash
+
+
+# Your code after this point
+
+def movies_with_director_key(name, movies_collection)
+  data = []
+  movies_collection.length.times do |index|
+    data[index] = movie_with_director_name(name, movies_collection[index])
+  end
+  puts data 
+end
+
